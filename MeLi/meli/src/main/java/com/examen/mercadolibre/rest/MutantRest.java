d43df0473cd0516244d4b5ddfc665720eef60693
Elizabeth <elizabeth.a.gutierrez.l@gmail.com>
2022-05-17 01:47:11 -0400

    Update - Test
    Se Agregan pruebas unitarias

--- MeLi/meli/src/main/java/com/examen/mercadolibre/rest/MutantRest.java
+++ MeLi/meli/src/main/java/com/examen/mercadolibre/rest/MutantRest.java
@@ -1,5 +1,7 @@
 package com.examen.mercadolibre.rest;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,37 +14,51 @@ import org.springframework.web.bind.annotation.RestController;
 import com.examen.mercadolibre.DTO.DnaDTO;
 import com.examen.mercadolibre.model.Dna;
 import com.examen.mercadolibre.model.Stats;
-import com.examen.mercadolibre.service.DnaService;
+import com.examen.mercadolibre.repository.DnaRepository;
 import com.examen.mercadolibre.service.MutantService;
 
 @RestController
 @RequestMapping("")
 public class MutantRest {
+	
+	@Autowired
+	MutantService mutantService = new MutantService();
 
-	@Autowired
-	MutantService mutantService;
 	
-	@Autowired
-	DnaService dnaService;
+	@Autowired 
+	DnaRepository dnaRepository;
 	
+	public MutantRest(DnaRepository dnaRepository) {
+		this.dnaRepository = dnaRepository;
+	}
+
 	@PostMapping("/mutant/")
-	public ResponseEntity<String[]> getMutant(@RequestBody DnaDTO dna) {
-		if (mutantService.isMutant(dna.getDna())) {
-			dnaService.createDna(new Dna(dna.getDna().toString(), true));
-			return new ResponseEntity<String[]>(HttpStatus.OK);
-		}else {
-			dnaService.createDna(new Dna(dna.getDna().toString(), false));
-			return new ResponseEntity<String[]>(HttpStatus.FORBIDDEN);
+	public ResponseEntity<String> getMutant(@RequestBody DnaDTO dna) throws IOException{
+		try {
+
+			if (mutantService.isMutant(dna)) {
+				dnaRepository.save(new Dna(dna.getDna().toString(), true));
+				return new ResponseEntity<String>(HttpStatus.OK);
+			}else {
+				dnaRepository.save(new Dna(dna.getDna().toString(), false));	
+				return new ResponseEntity<String>(HttpStatus.FORBIDDEN);
+			}
+				
+		}catch (Exception e){
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
 	
 	@GetMapping("/stats")
-	public ResponseEntity<Stats> getStats() {
-		Long countMutant = dnaService.getMutantDnaCount();
-		Long countHuman = dnaService.getHumanDnaCount();
-		double radio = countMutant * 1.0 / countHuman;
-		Stats stats = new Stats(countMutant, countHuman, radio);
-		return new ResponseEntity<Stats>(stats, HttpStatus.OK);
-		
+	public ResponseEntity<Stats> getStats(){
+		try { 
+			Long countMutant = dnaRepository.countByDnaMutant(true);
+			Long countHuman = dnaRepository.countByDnaMutant(false);
+			double ratio = countMutant * 1.0 / countHuman;
+			Stats stats = new Stats(countMutant, countHuman, ratio);
+			return new ResponseEntity<Stats>(stats, HttpStatus.OK);
+		}catch (Exception e) {
+			return new ResponseEntity<Stats>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
--- MeLi/meli/src/test/java/com/examen/mercadolibre/MeliApplicationTests.java
+++ MeLi/meli/src/test/java/com/examen/mercadolibre/MeliApplicationTests.java
@@ -1,13 +1,110 @@
 package com.examen.mercadolibre;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MeliApplicationTests {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import com.examen.mercadolibre.DTO.DnaDTO;
+import com.examen.mercadolibre.model.Dna;
+import com.examen.mercadolibre.model.Stats;
+import com.examen.mercadolibre.repository.DnaRepository;
+import com.examen.mercadolibre.rest.MutantRest;
+
+
+public class MeliApplicationTests {
+/*
+	@Autowired
+	public MutantRest mutantRest = new MutantRest();
+*/
+	
+
+	DnaRepository dnaRepositoryMock = Mockito.mock(DnaRepository.class);
+
+	@Autowired
+	MutantRest mutantRest = new MutantRest(dnaRepositoryMock);
 
 	@Test
-	void contextLoads() {
+	void dnaTestEsMutante() throws Exception {
+
+		String[] dna = {"AAAAT", "CCCAT", "CATCT", "TCGAT", "TGACT"};
+		DnaDTO adnDTO = new DnaDTO();
+		adnDTO.setDna(dna);
+		Dna mockDna = new Dna(dna.toString(),true);
+		Mockito.when(dnaRepositoryMock.save(mockDna)).thenReturn(mockDna);
+		ResponseEntity<String> response =  mutantRest.getMutant(adnDTO);
+
+		Assertions.assertEquals(200,response.getStatusCodeValue());		
+	}
+	
+	@Test
+	void dnaTestNoEsMutante() throws Exception {
+
+		String[] dna = {"AACAT", "CCCAT", "CATCT", "TCGAT", "TGACT"};
+		DnaDTO adnDTO = new DnaDTO();
+		adnDTO.setDna(dna);
+		Dna mockDna = new Dna(dna.toString(),true);
+		Mockito.when(dnaRepositoryMock.save(mockDna)).thenReturn(mockDna);
+		ResponseEntity<String> response =  mutantRest.getMutant(adnDTO);
+
+		Assertions.assertEquals(403,response.getStatusCodeValue());		
+	}
+	
+	@Test
+	void dnaTestNoEsMatrizValida() throws Exception {
+
+		String[] dna = {"AACATTTT", "CCCAT", "CATCT", "TCGAT", "TGACT"};
+		DnaDTO adnDTO = new DnaDTO();
+		adnDTO.setDna(dna);
+		Dna mockDna = new Dna(dna.toString(),true);
+		Mockito.when(dnaRepositoryMock.save(mockDna)).thenReturn(mockDna);
+		ResponseEntity<String> response =  mutantRest.getMutant(adnDTO);
+
+		Assertions.assertEquals(400,response.getStatusCodeValue());		
+	}
+	
+	@Test
+	void dnaTestTieneCaracteresNoAceptados() throws Exception {
+
+		String[] dna = {"AACAT", "CCCBT", "CATCT", "TCGAT", "TGACT"};
+		DnaDTO adnDTO = new DnaDTO();
+		adnDTO.setDna(dna);
+		Dna mockDna = new Dna(dna.toString(),true);
+		Mockito.when(dnaRepositoryMock.save(mockDna)).thenReturn(mockDna);
+		ResponseEntity<String> response =  mutantRest.getMutant(adnDTO);
+
+		Assertions.assertEquals(400,response.getStatusCodeValue());		
+	}
+	
+	@Test
+	void dnaTestDnaNull() throws Exception {
+		DnaDTO adnDTO = new DnaDTO();
+		adnDTO.setDna(null);
+		Dna mockDna = new Dna(null,true);
+		Mockito.when(dnaRepositoryMock.save(mockDna)).thenReturn(mockDna);
+		ResponseEntity<String> response =  mutantRest.getMutant(adnDTO);
+
+		Assertions.assertEquals(400,response.getStatusCodeValue());		
+	}
+	
+	@Test
+	void dnaTestDnaNull2() throws Exception {
+
+		DnaDTO adnDTO = null;
+		Dna mockDna = null;
+		Mockito.when(dnaRepositoryMock.save(mockDna)).thenReturn(mockDna);
+		ResponseEntity<String> response =  mutantRest.getMutant(adnDTO);
+
+		Assertions.assertEquals(400,response.getStatusCodeValue());		
 	}
 
+
+	@Test
+	public void testStatOk() throws Exception{ 
+		ResponseEntity<Stats> response = mutantRest.getStats();
+		Assertions.assertEquals(200,response.getStatusCodeValue());	
+
+	}
 }
